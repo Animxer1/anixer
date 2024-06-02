c9156f8a1e6b79a23fdc8eef3bb7028fd4d074fe
Animxer1 <134288691+Animxer1@users.noreply.github.com>
2024-06-02 21:32:24 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,8 +6,7 @@ const recentapi = "/recent/";
 
 // Api Server Manager
 
-const AvailableServers = ['https://animedexapi.xeranime707.workers.dev', 'https://animedexapi.rexcalumba86.workers.dev', 'https://animedexapi.xeranime707.workers.dev']
-
+const AvailableServers = ["https://animedex-api.animxersite.workers.dev"];
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)]
 }
